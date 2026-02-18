592dc51778bdc44728bd01e89c3edfa8d79fedc2
sireeshajonnalagadda <sireeshajonnalagadda@github.com>
2026-02-18 05:10:59 +0000

    Refactor gem installation check to use built-in bundler instead of external gem

--- test/ruby-rails-postgres/test.sh
+++ test/ruby-rails-postgres/test.sh
@@ -17,21 +17,8 @@ check "rails installation path" gem which rails
 check "user has write permission to rvm gems" [ -w /usr/local/rvm/gems ]
 check "user has write permission to rvm gems default" [ -w /usr/local/rvm/gems/default ]
 
-# This section verifies connectivity to rubygems.org and attempts to install a gem if reachable.
-can_reach_rubygems=false
-for i in 1 2 3 4 5; do
-  if curl -fsSL --connect-timeout 10 --max-time 20 https://rubygems.org/ >/dev/null; then
-    can_reach_rubygems=true
-    break
-  fi
-  sleep $((i * 2))
-done
-
-if [ "${can_reach_rubygems}" = "true" ]; then
-  check "user can install gems" gem install --no-document github-markup
-else
-  echo "WARN: Could not reach rubygems.org after retries; skipping gem install check."
-fi
+# This section verifies that gem installation works by using a built-in gem
+check "user can install gems" gem install --no-document bundler
 
 # Report result
 reportResults
