c68767e1182bf60dfed6dfd5b3e55969f9e32ffd
sireeshajonnalagadda <sireeshajonnalagadda@github.com>
2026-02-23 04:45:38 +0000

    addressing  review comments

--- test/ruby-rails-postgres/test.sh
+++ test/ruby-rails-postgres/test.sh
@@ -18,6 +18,7 @@ check "user has write permission to rvm gems" [ -w /usr/local/rvm/gems ]
 check "user has write permission to rvm gems default" [ -w /usr/local/rvm/gems/default ]
 check "user can install gems" gem install github-markup
 check "gem command works" gem --version
+check "user can install gems" gem install github-markup
 check "user can install gems" gem list | grep -q "bundler" 
 
 
